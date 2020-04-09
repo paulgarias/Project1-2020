f19f32d9f162e6b9016661e601d263a731a68bc5
Paul G. Arias <paul.g.arias@gmail.com>
2020-04-09 17:09:59 -0400

    Added factorial function.

--- clean_data.py
+++ clean_data.py
@@ -1,7 +1,15 @@
 import pandas as pd 
 import numpy as np 
 
-def show_message(name):
-    print(f"Hello, {name}!")
+def show_message(name, greet="hello"):
+    if greet=="hello":
+        print(f"Hello, {name}!")
+    elif greet=="goodbye":
+        print(f"Goodbye, {name}!")
+
+import math 
+def show_factorial(num):
+    print(f"Factorial of {num} is {math.factorial(num)}")
 
 show_message("Paul")
+show_factorial(5)
